655612206e7f6810dbb37ad840ffa06b6d8f6162
Joakim Joensuu <joakim.joensuu@gmail.com>
2021-06-04 07:43:01 +0300

    Add isochrone calculation, mean time calculation and plotting

--- heatmap/__main__.py
+++ heatmap/__main__.py
@@ -1,194 +1,151 @@
-from shapely.geometry import Polygon, Point, shape
-from shapely.ops import unary_union, transform
+from json.decoder import JSONDecodeError
+from geopandas.geodataframe import GeoDataFrame
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
 import geopandas as gpd
+from geopandas import GeoDataFrame
 import requests
-from pyproj import Transformer
-from typing import List
-import numpy
-import time
-from multiprocessing import Pool, cpu_count
-import math
 from geopy.geocoders import Nominatim
 import requests
 import json
+from typing import List
 
 
-def get_hsl_polygon() -> Polygon:
-
-    hsl_cities: list[str] = [
-        "Helsinki",
-        "Espoo",
-        "Vantaa",
-        "Kauniainen",
-        "Siuntio",
-        "Kirkkonummi",
-        "Sipoo",
-        "Kerava",
-        "Tuusula",
-    ]
-
-    city_boundary_request_address: str = "https://nominatim.openstreetmap.org/search.php?city={}&polygon_geojson=1&format=json&limit=1"
-
-    city_polygons: list[Polygon] = []
-
-    for city in hsl_cities:
-        final_request_address: str = city_boundary_request_address.format(city)
-        request: requests.models.Response = requests.get(final_request_address)
-        city_polygon_data: list[list[list[float]]] = request.json()[0]["geojson"][
-            "coordinates"
-        ]
-        polygon_shell: list[list[float]] = city_polygon_data[0]
-        polygon_holes: list[list[float]] = city_polygon_data[1:]
-        city_polygon = Polygon(shell=polygon_shell, holes=polygon_holes)
-        city_polygons.append(city_polygon)
-
-    hsl_polygon = unary_union(city_polygons)
-
-    return hsl_polygon
-
-
-def point_within_polygon(params):
-    x_min, x_max, y_min, y_max, spacing_in_meters, proxy_polygon_to_intersect = params
-
-    to_original_transformer = Transformer.from_crs("epsg:3857", "epsg:4326")
-
-    number_of_points = (int((x_max - x_min) / spacing_in_meters) + 1) * (
-        int((y_max - y_min) / spacing_in_meters) + 1
-    )
-
-    gridpoints = numpy.empty(number_of_points, dtype=Point)
-    i = 0
-
-    x = x_min
-    while x < x_max:
-        y = y_min
-        while y < y_max:
-            p = Point(x, y)
-            if proxy_polygon_to_intersect.intersects(p):
-                lat, lon = to_original_transformer.transform(x, y)
-                gridpoints[i] = Point(lon, lat)
-                i = i + 1
-
-            y += spacing_in_meters
-        x += spacing_in_meters
-
-    return gridpoints[:i]
-
-
-def get_point_grid(
-    lon_min,
-    lat_min,
-    lon_max,
-    lat_max,
-    spacing_in_meters: int,
-    polygon_to_intersect: Polygon,
-) -> List[Polygon]:
-    print("start")
-    start = time.time()
-
-    to_proxy_transformer = Transformer.from_crs("epsg:4326", "epsg:3857")
-
-    x_min, y_min = to_proxy_transformer.transform(lat_min, lon_min)
-    x_max, y_max = to_proxy_transformer.transform(lat_max, lon_max)
-
-    polygon_transformer = Transformer.from_crs("epsg:4326", "epsg:3857", always_xy=True)
-    proxy_polygon_to_intersect = transform(
-        polygon_transformer.transform,
-        polygon_to_intersect,
-    )
-
-    worker_count = cpu_count()
-
-    difference = x_max - x_min
-    subintervals = math.floor(difference / spacing_in_meters)
-    subintervals_per_interval, excess_subintervals = divmod(subintervals, worker_count)
-    interval_lengths = [
-        spacing_in_meters * subintervals_per_interval
-        + spacing_in_meters * (i < excess_subintervals)
-        for i in range(worker_count)
-    ]
-
-    intervals = [x_min] * (worker_count + 1)
-
-    for i, interval_length in enumerate(interval_lengths):
-        intervals[i + 1] = intervals[i] + interval_length
-
-    interval_pairs = list(zip(intervals[:-1], intervals[1:]))
-
-    parameters = [
-        [*xs, y_min, y_max, spacing_in_meters, proxy_polygon_to_intersect]
-        for xs in interval_pairs
-    ]
-
-    with Pool(worker_count) as pool:
-        results = numpy.concatenate(
-            pool.map(point_within_polygon, parameters), axis=None, dtype=Point
-        )
-
-    end = time.time()
-    print("time", end - start)
-
-    return results
-
-
-def do_the_shit():
-
-    hsl_polygon: Polygon = get_hsl_polygon()
-
-    spacing = 100
-
-    hsl_dot_grid: List[Polygon] = get_point_grid(
-        *hsl_polygon.bounds, spacing, hsl_polygon
-    )
-    print("Amount of points in the polygon:", len(hsl_dot_grid))
-
-    p = gpd.GeoSeries(hsl_polygon)
-    p.plot()
-
-    xs = [point.x for point in hsl_dot_grid]
-    ys = [point.y for point in hsl_dot_grid]
-    plt.scatter(xs, ys, color="orange", s=rcParams["lines.markersize"] ** 0.5)
-    plt.show()
-
-
-def api_for_minutes(minutes):
-    api_url = "http://localhost:8080/otp/routers/hsl/isochrone?fromPlace={},{}&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=4:00pm&date=05-31-2021"
+def api_url_from_for_isochrones(minutes):
+    api_url = "http://localhost:8080/otp/routers/hsl/isochrone?fromPlace={},{}&precisionMeters=100&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=08:00am&date=06-03-2021"
     for minute in minutes:
         api_url = f"{api_url}&cutoffSec={minute*60}"
 
     return api_url
 
 
-def plot_heatmap(location):
+def api_url_to_for_isochrones(minutes):
+    api_url = "http://localhost:8080/otp/routers/hsl/isochrone?toPlace={},{}&fromPlace={},{}&precisionMeters=100&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=08:00am&date=06-03-2021"
+    for minute in minutes:
+        api_url = f"{api_url}&cutoffSec={minute*60}"
 
-    api_url = api_for_minutes(range(6, 240, 1))
+    return api_url
+
+
+def get_isochrone(coordinates) -> GeoDataFrame:
+
+    api_url_from = api_url_from_for_isochrones(range(0, 40 + 1, 1))
 
     featurecollection = requests.get(
-        api_url.format(location.latitude, location.longitude)
+        api_url_from.format(
+            coordinates.latitude,
+            coordinates.longitude,
+        )
     ).json()
 
-    with open("temp.txt", "w") as outfile:
-        json.dump(featurecollection, outfile, indent=2)
+    gdf_from = gpd.GeoDataFrame.from_features(featurecollection["features"]).dropna()
 
-    gdf = gpd.GeoDataFrame.from_features(featurecollection["features"])
+    gdf_from["time"] = gdf_from["time"].astype(float)
 
-    gdf.plot(column="time")
+    for i in range(0, len(gdf_from) - 1):
+        gdf_from.iloc[[i]] = gpd.overlay(
+            gdf_from.iloc[[i]], gdf_from.iloc[[i + 1]], how="difference"
+        )
+
+    api_url_to = api_url_to_for_isochrones(range(0, 30 + 1, 1))
+
+    featurecollection = requests.get(
+        api_url_to.format(
+            coordinates.latitude,
+            coordinates.longitude,
+            coordinates.latitude,
+            coordinates.longitude,
+        )
+    ).json()
+
+    gdf_to = gpd.GeoDataFrame.from_features(featurecollection["features"]).dropna()
+
+    gdf_to["time"] = gdf_to["time"].astype(float)
+
+    for i in range(0, len(gdf_to) - 1):
+        gdf_to.iloc[[i]] = gpd.overlay(
+            gdf_to.iloc[[i]], gdf_to.iloc[[i + 1]], how="difference"
+        )
+
+    isochrones_union = gdf_to
+
+    isochrone = gdf_from
+
+    isochrones_union = gpd.overlay(
+        isochrones_union, isochrone, how="intersection", keep_geom_type=True
+    )
+
+    isochrones_union["time"] = isochrones_union["time_1"] + isochrones_union["time_2"]
+
+    isochrones_union["time"] = isochrones_union["time"] / 2
+
+    isochrones_union = isochrones_union.drop(columns=["time_1", "time_2"])
+
+    return isochrones_union
 
 
 if __name__ == "__main__":
+    locations = [
+        "Haartmaninkatu 8, 00100 Helsinki",
+        "Maarintie 6, 02150 Espoo",
+        "Rautatientori, Helsinki",
+    ]
+
     geolocator = Nominatim(user_agent="Google Maps")
-    location = geolocator.geocode("Pietari Kalmin Katu 1, Helsinki, Finland")
+    locations_coordinates = [geolocator.geocode(location) for location in locations]
 
-    plot_heatmap(location)
+    isochrones: List[GeoDataFrame] = [
+        get_isochrone(coordinates) for coordinates in locations_coordinates
+    ]
 
-    location = geolocator.geocode("Siilitie 8, Helsinki, Finland")
+    print("Intersecting isochrones. This will take some time.")
 
-    plot_heatmap(location)
+    isochrones_union = isochrones[0]
 
+    for i, isochrone in enumerate(isochrones[1:]):
+        isochrones_union = gpd.overlay(
+            isochrones_union, isochrone, how="intersection", keep_geom_type=True
+        )
+
+        isochrones_union["time_1"] = isochrones_union["time_1"] * (i + 1)
+
+        isochrones_union["time"] = (
+            isochrones_union["time_1"] + isochrones_union["time_2"]
+        )
+
+        isochrones_union["time"] = isochrones_union["time"] / (i + 2)
+
+        isochrones_union = isochrones_union.drop(columns=["time_1", "time_2"])
+
+    isochrones_union["time"] = isochrones_union["time"] / 60
+
+    isochrones_union = isochrones_union.set_crs("epsg:4326").to_crs("EPSG:3857")
+
+    print(isochrones_union)
+
+    ax = isochrones_union.plot(column="time", legend=True, alpha=0.7, cmap="autumn")
+
+    # 2703424.82,8394314.42,2812118.27,8481605.53
+    background = plt.imread("./heatmap/2703424.82,8394314.42,2812118.27,8481605.53.png")
+    bbox = [2703424.82, 2812118.27, 8394314.42, 8481605.53]
+    ax.set_xlim(bbox[0], bbox[1])
+    ax.set_ylim(bbox[2], bbox[3])
+    ax.imshow(background, zorder=0, extent=bbox, aspect="equal")
+
+    plt.tight_layout()
     plt.show()
 
-    exit()
+    # (24.0082613, 59.7915557, 25.5269348, 60.5806498)
+    # for lat: f'{number:.15f}'
+    # for lon: f'{number:.13f}'
+    # https://render.openstreetmap.org/cgi-bin/export?bbox=24.285278320312504,59.97563121816456,25.26168823242188,60.36567607497561&scale=175000&format=png
 
-    do_the_shit()
+    # lon_diff = (25.5230000000000 - 24.0000000000000) / 100
+    # lat_diff = (60.59000000000000 - 59.90000000000000) / 100
+
+    # lon_min = 24.0000000000000
+    # lat_min = 59.90000000000000
+    # lon_max = lon_min + lon_diff
+    # lat_max = lat_min + lat_diff
+
+    # while lon_min <= 25.5230000000000:
+    #    print(f"{lon_min:.13f}")
