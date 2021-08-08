037308e64999b114af7058257cf01c2386e929ac
Joakim Joensuu <joakim.joensuu@gmail.com>
2021-08-08 19:17:19 +0300

    Add dynamic background and refactor

--- heatmap/__main__.py
+++ heatmap/__main__.py
@@ -1,151 +1,305 @@
-from json.decoder import JSONDecodeError
-from geopandas.geodataframe import GeoDataFrame
+import argparse
+import decimal
+import sys
+import time
+from argparse import Namespace
+from multiprocessing import Lock, Manager, Pool, Process, cpu_count
+from multiprocessing.managers import NamespaceProxy
+from typing import List, Tuple
+
 import matplotlib.pyplot as plt
-import geopandas as gpd
-from geopandas import GeoDataFrame
+import numpy as np
+import pandas as pd
 import requests
+from geopandas import GeoDataFrame, overlay
 from geopy.geocoders import Nominatim
-import requests
-import json
-from typing import List
+from geopy.location import Location
+from matplotlib.axes import Axes
+from numpy import add, arange, exp
+from PIL import Image
+from PIL.PngImagePlugin import PngImageFile
+from pyproj import Transformer
+from requests import get
+
+ISOCHRONE_REQUEST_BASE_URL = "http://localhost:8080/otp/routers/hsl/isochrone"
+
+ISOCHRONE_REQUEST_URL_PARAMETERS = "&precisionMeters=100&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=10:00am&date=08-08-2021"
 
 
-def api_url_from_for_isochrones(minutes):
-    api_url = "http://localhost:8080/otp/routers/hsl/isochrone?fromPlace={},{}&precisionMeters=100&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=08:00am&date=06-03-2021"
-    for minute in minutes:
-        api_url = f"{api_url}&cutoffSec={minute*60}"
-
-    return api_url
-
-
-def api_url_to_for_isochrones(minutes):
-    api_url = "http://localhost:8080/otp/routers/hsl/isochrone?toPlace={},{}&fromPlace={},{}&precisionMeters=100&mode=WALK,TRAM,TRANSIT,BUS,SUBWAY,RAIL,FERRY&time=08:00am&date=06-03-2021"
-    for minute in minutes:
-        api_url = f"{api_url}&cutoffSec={minute*60}"
-
-    return api_url
+def round_down(value, decimals):
+    with decimal.localcontext() as ctx:
+        d = decimal.Decimal(value)
+        ctx.rounding = decimal.ROUND_DOWN
+        return float(round(d, decimals))
 
 
-def get_isochrone(coordinates) -> GeoDataFrame:
+def round_up(value, decimals):
+    with decimal.localcontext() as ctx:
+        d = decimal.Decimal(value)
+        ctx.rounding = decimal.ROUND_CEILING
+        return float(round(d, decimals))
 
-    api_url_from = api_url_from_for_isochrones(range(0, 40 + 1, 1))
 
-    featurecollection = requests.get(
-        api_url_from.format(
-            coordinates.latitude,
-            coordinates.longitude,
-        )
-    ).json()
+def plot_heatmap(mean_travel_times: GeoDataFrame) -> Axes:
+    geodetic_bounds = mean_travel_times.total_bounds
 
-    gdf_from = gpd.GeoDataFrame.from_features(featurecollection["features"]).dropna()
-
-    gdf_from["time"] = gdf_from["time"].astype(float)
-
-    for i in range(0, len(gdf_from) - 1):
-        gdf_from.iloc[[i]] = gpd.overlay(
-            gdf_from.iloc[[i]], gdf_from.iloc[[i + 1]], how="difference"
-        )
-
-    api_url_to = api_url_to_for_isochrones(range(0, 30 + 1, 1))
-
-    featurecollection = requests.get(
-        api_url_to.format(
-            coordinates.latitude,
-            coordinates.longitude,
-            coordinates.latitude,
-            coordinates.longitude,
-        )
-    ).json()
-
-    gdf_to = gpd.GeoDataFrame.from_features(featurecollection["features"]).dropna()
-
-    gdf_to["time"] = gdf_to["time"].astype(float)
-
-    for i in range(0, len(gdf_to) - 1):
-        gdf_to.iloc[[i]] = gpd.overlay(
-            gdf_to.iloc[[i]], gdf_to.iloc[[i + 1]], how="difference"
-        )
-
-    isochrones_union = gdf_to
-
-    isochrone = gdf_from
-
-    isochrones_union = gpd.overlay(
-        isochrones_union, isochrone, how="intersection", keep_geom_type=True
+    geodetic_bounds = (
+        round_down(geodetic_bounds[0], 4),
+        round_down(geodetic_bounds[1], 4),
+        round_up(geodetic_bounds[2], 4),
+        round_up(geodetic_bounds[3], 4),
     )
 
-    isochrones_union["time"] = isochrones_union["time_1"] + isochrones_union["time_2"]
+    print(geodetic_bounds)
 
-    isochrones_union["time"] = isochrones_union["time"] / 2
+    images: List[List[PngImageFile]] = []
 
-    isochrones_union = isochrones_union.drop(columns=["time_1", "time_2"])
+    token = requests.get("https://www.openstreetmap.org/").cookies.get(
+        "_osm_totp_token"
+    )
 
-    return isochrones_union
+    lon_min = geodetic_bounds[0]
+    lat_min = geodetic_bounds[1]
 
+    counter = 0
+    step = 0.07
 
-if __name__ == "__main__":
-    locations = [
-        "Haartmaninkatu 8, 00100 Helsinki",
-        "Maarintie 6, 02150 Espoo",
-        "Rautatientori, Helsinki",
+    for lat_max in arange(geodetic_bounds[1], geodetic_bounds[3] + step, step)[1:]:
+        row: List[PngImageFile] = []
+        for lon_max in arange(geodetic_bounds[0], geodetic_bounds[2] + step, step)[1:]:
+            if lat_max > geodetic_bounds[3]:
+                lat_max = geodetic_bounds[3]
+            if lon_max > geodetic_bounds[2]:
+                lon_max = geodetic_bounds[2]
+
+            print(
+                f"https://render.openstreetmap.org/cgi-bin/export?bbox={lon_min},{lat_min},{lon_max},{lat_max}&scale=20000&format=png"
+            )
+
+            try:
+                response = requests.get(
+                    f"https://render.openstreetmap.org/cgi-bin/export?bbox={lon_min},{lat_min},{lon_max},{lat_max}&scale=20000&format=png",
+                    headers={"Cookie": f"_osm_totp_token={token}"},
+                    stream=True,
+                )
+
+                response.raise_for_status()
+            except requests.exceptions.HTTPError:
+                print(response.content)
+                exit()
+
+            row.append(Image.open(response.raw))
+
+            print(row[-1].size)
+
+            counter += 1
+
+            lon_min = lon_max
+        images.append(row)
+        lat_min = lat_max
+        lon_min = geodetic_bounds[0]
+
+    width = sum([image.size[0] for image in images[0]])
+
+    height = sum(row[0].size[1] for row in images)
+
+    print(width)
+    print(height)
+
+    new_image = Image.new("RGB", (width, height))
+
+    x = 0
+    y = 0
+
+    for row in reversed(images):
+        for image in row:
+            new_image.paste(image, (x, y))
+
+            x += image.size[0]
+
+        y += row[0].size[1]
+        x = 0
+
+    mean_travel_times = mean_travel_times.to_crs("EPSG:3857")
+
+    ax = mean_travel_times.plot(
+        column="average_time", legend=True, alpha=0.7, cmap="autumn"
+    )
+
+    to_mercantor = Transformer.from_crs("EPSG:4326", "EPSG:3857")
+    mercantor_bounds = [
+        *to_mercantor.transform(geodetic_bounds[1], geodetic_bounds[0]),
+        *to_mercantor.transform(geodetic_bounds[3], geodetic_bounds[2]),
     ]
 
-    geolocator = Nominatim(user_agent="Google Maps")
-    locations_coordinates = [geolocator.geocode(location) for location in locations]
-
-    isochrones: List[GeoDataFrame] = [
-        get_isochrone(coordinates) for coordinates in locations_coordinates
+    background = np.asarray(new_image)
+    bbox = [
+        mercantor_bounds[0],
+        mercantor_bounds[2],
+        mercantor_bounds[1],
+        mercantor_bounds[3],
     ]
-
-    print("Intersecting isochrones. This will take some time.")
-
-    isochrones_union = isochrones[0]
-
-    for i, isochrone in enumerate(isochrones[1:]):
-        isochrones_union = gpd.overlay(
-            isochrones_union, isochrone, how="intersection", keep_geom_type=True
-        )
-
-        isochrones_union["time_1"] = isochrones_union["time_1"] * (i + 1)
-
-        isochrones_union["time"] = (
-            isochrones_union["time_1"] + isochrones_union["time_2"]
-        )
-
-        isochrones_union["time"] = isochrones_union["time"] / (i + 2)
-
-        isochrones_union = isochrones_union.drop(columns=["time_1", "time_2"])
-
-    isochrones_union["time"] = isochrones_union["time"] / 60
-
-    isochrones_union = isochrones_union.set_crs("epsg:4326").to_crs("EPSG:3857")
-
-    print(isochrones_union)
-
-    ax = isochrones_union.plot(column="time", legend=True, alpha=0.7, cmap="autumn")
-
-    # 2703424.82,8394314.42,2812118.27,8481605.53
-    background = plt.imread("./heatmap/2703424.82,8394314.42,2812118.27,8481605.53.png")
-    bbox = [2703424.82, 2812118.27, 8394314.42, 8481605.53]
     ax.set_xlim(bbox[0], bbox[1])
     ax.set_ylim(bbox[2], bbox[3])
     ax.imshow(background, zorder=0, extent=bbox, aspect="equal")
 
-    plt.tight_layout()
+    return ax
+
+
+def mean_time_calculating_worker(ns: NamespaceProxy, lock: Lock):
+    while True:
+        lock.acquire()
+
+        if len(ns.travel_times) < 2:
+            lock.release()
+            return
+
+        first = ns.travel_times[-1]
+        second = ns.travel_times[-2]
+        ns.travel_times = ns.travel_times[:-2]
+
+        lock.release()
+
+        average_times = overlay(first, second, how="intersection", keep_geom_type=True)
+
+        if len(average_times.index) == 0:
+            continue
+
+        average_times["average_time"] = (
+            average_times["average_time_1"] * average_times["divider_1"]
+            + average_times["average_time_2"] * average_times["divider_2"]
+        ) / (average_times["divider_1"] + average_times["divider_2"])
+
+        average_times["divider"] = (
+            average_times["divider_1"] + average_times["divider_2"]
+        )
+
+        average_times = average_times.drop(
+            columns=["average_time_1", "divider_1", "average_time_2", "divider_2"]
+        )
+
+        lock.acquire()
+        ns.travel_times = ns.travel_times + [average_times]
+        lock.release()
+
+
+def calculate_mean_times(travel_times: GeoDataFrame) -> GeoDataFrame:
+    lock = Lock()
+    ns: NamespaceProxy = Manager().Namespace()
+    ns.travel_times = travel_times
+
+    processes: List[Process] = [
+        Process(target=mean_time_calculating_worker, args=(ns, lock))
+        for _ in range(cpu_count())
+    ]
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    return ns.travel_times[0].drop(columns=["divider"])
+
+
+def request_isochrone(request_url: str) -> GeoDataFrame:
+
+    print(get(request_url).request)
+
+    isochrone: GeoDataFrame = (
+        GeoDataFrame.from_features(get(request_url).json().get("features"))
+        .dropna()
+        .rename(columns={"time": "average_time"})
+        .astype({"average_time": float})
+        .set_crs("epsg:4326")
+    )
+
+    for i in range(0, len(isochrone.index) - 1):
+        isochrone.iloc[[i]] = overlay(
+            isochrone.iloc[[i]], isochrone.iloc[[i + 1]], how="difference"
+        )
+
+    isochrone["divider"] = 1
+    return isochrone
+
+
+def request_isochrones(request_urls: List[str]) -> List[GeoDataFrame]:
+    return Pool(cpu_count()).map(request_isochrone, request_urls)
+
+
+def url_cutoff_parameters(cutoff_time: int, cutoff_step: int) -> List[str]:
+    return "".join(
+        [
+            f"&cutoffSec={minute*60}"
+            for minute in range(0, cutoff_time + cutoff_step, cutoff_step)
+        ]
+    )
+
+
+def from_url(coords: Location) -> str:
+    return f"{ISOCHRONE_REQUEST_BASE_URL}?fromPlace={coords.latitude},{coords.longitude}{ISOCHRONE_REQUEST_URL_PARAMETERS}"
+
+
+def to_url(coords: Location) -> str:
+    return f"{ISOCHRONE_REQUEST_BASE_URL}?toPlace={coords.latitude},{coords.longitude}&fromPlace={coords.latitude},{coords.longitude}{ISOCHRONE_REQUEST_URL_PARAMETERS}"
+
+
+def coordinates_to_request_urls(
+    locations: List[Location],
+    cutoff_time: int,
+    cutoff_step: int,
+) -> List[str]:
+    return [
+        url(coords) + url_cutoff_parameters(cutoff_time, cutoff_step)
+        for coords in locations
+        for url in (from_url, to_url)
+    ]
+
+
+def addresses_to_coordinates(addresses: List[str]) -> List[Location]:
+    geolocator: Nominatim = Nominatim(user_agent="Google Maps")
+    return [geolocator.geocode(address) for address in addresses]
+
+
+def main(addresses: List[str], cutoff_time: int, cutoff_step: int) -> None:
+
+    print(f"Got {len(addresses)} places:")
+    [print(f"  - {place}") for place in addresses]
+
+    location_coordinates: List[Location] = addresses_to_coordinates(addresses)
+
+    print("Found:")
+    [print(f"  - {coordinates}") for coordinates in location_coordinates]
+
+    request_urls: List[str] = coordinates_to_request_urls(
+        location_coordinates, cutoff_time, cutoff_step
+    )
+
+    print(f"Requesting isochrone data")
+
+    travel_times: List[GeoDataFrame] = request_isochrones(request_urls)
+
+    print("Calculating mean times")
+
+    mean_travel_times: GeoDataFrame = calculate_mean_times(travel_times)
+
+    print("Plotting")
+
+    ax: Axes = plot_heatmap(mean_travel_times)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "addresses",
+        type=str,
+        nargs="+",
+    )
+    parser.add_argument("--cutoff-time", type=int, default=30)
+    parser.add_argument("--cutoff-step", type=int, default=1)
+
+    args: Namespace = parser.parse_args()
+
+    main(args.addresses, args.cutoff_time, args.cutoff_step)
+
     plt.show()
-
-    # (24.0082613, 59.7915557, 25.5269348, 60.5806498)
-    # for lat: f'{number:.15f}'
-    # for lon: f'{number:.13f}'
-    # https://render.openstreetmap.org/cgi-bin/export?bbox=24.285278320312504,59.97563121816456,25.26168823242188,60.36567607497561&scale=175000&format=png
-
-    # lon_diff = (25.5230000000000 - 24.0000000000000) / 100
-    # lat_diff = (60.59000000000000 - 59.90000000000000) / 100
-
-    # lon_min = 24.0000000000000
-    # lat_min = 59.90000000000000
-    # lon_max = lon_min + lon_diff
-    # lat_max = lat_min + lat_diff
-
-    # while lon_min <= 25.5230000000000:
-    #    print(f"{lon_min:.13f}")
